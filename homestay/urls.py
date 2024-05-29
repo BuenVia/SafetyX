72b17f6740ac5fe198b48585130eba85c4ef5d16
Matt Clifford <matthew.clifford@hfx.co.uk>
2024-05-29 14:08:52 +0100

    update to registration and authentication functionality

--- homestay/urls.py
+++ homestay/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name='index')
+    path("", views.index, name='index'),
+    path("register", views.register, name='register'),
+    path("login", views.login, name='login'),
+    path("dashboard", views.dashboard, name='dashboard'),
+    path("logout", views.logout, name='logout'),
 ]
--- homestay/views.py
+++ homestay/views.py
@@ -1,5 +1,46 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import auth
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from . forms import CreateUserForm, LoginForm
 
 # Create your views here.
 def index(request):
     return render(request, 'homestay/index.html')
+
+def register(request):
+    form = CreateUserForm()
+    if request.method == "POST":
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("login")
+    context = {'registerform': form}
+    return render(request, 'homestay/register.html', context=context)
+
+
+def login(request):
+    if request.user.is_authenticated:
+        return redirect("dashboard")
+    else:
+        form = LoginForm()
+        if request.method == "POST":
+            form = LoginForm(request, data=request.POST)
+            if form.is_valid():
+                username = request.POST.get('username')
+                password = request.POST.get('password')
+                user = authenticate(request, username=username, password=password)
+                if user is not None:
+                    auth.login(request, user)
+                    return redirect("dashboard")
+        context = {'loginform': form}
+        return render(request, 'homestay/login.html', context=context)
+
+def logout(request):
+    auth.logout(request)
+    return redirect("index")
+
+@login_required(login_url="login")
+def dashboard(request):
+    if request.user.is_authenticated:
+        return render(request, 'homestay/dashboard.html', {'username': request.user.username})
