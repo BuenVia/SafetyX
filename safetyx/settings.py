918e827147b046b4504d41f2deee7898439cf7e4
Matt Clifford <matthew.clifford@hfx.co.uk>
2024-05-29 09:11:39 +0100

    update

--- safetyx/settings.py
+++ safetyx/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'homestay.apps.HomestayConfig'
+    'homestay.apps.HomestayConfig',
+    'members.apps.MembersConfig',
 ]
 
 MIDDLEWARE = [
